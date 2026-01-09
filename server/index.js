ba6d432b38e858b0205d3eac7d0d4e550862d5df
ankitsjadhav <ankitsj7@gmail.com>
2026-01-09 13:19:54 +0530

    Configure PostgreSQL database connection using Prisma

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { connectDB } = require("./config/db");
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
