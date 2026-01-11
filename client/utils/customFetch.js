f3d496a90ebe56ab47a66eeda03737179cb00b9d
ankitsjadhav <ankitsj7@gmail.com>
2026-01-11 13:15:14 +0530

    backend url to fix connection

--- client/utils/customFetch.js
+++ client/utils/customFetch.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://store-rating-app-lac-seven.vercel.app/api";
+
 const customFetch = axios.create({
-  baseURL: "/api",
+  baseURL: BASE_URL,
 });
 
 export default customFetch;
